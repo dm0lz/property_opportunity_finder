db91a77208ec2c147246537063dd8a2e55133599
dm0lz <ducrouxolivier@gmail.com>
2019-09-15 19:18:23 +0200

    retrying failing task

--- app/services/importer/leboncoin/properties.rb
+++ app/services/importer/leboncoin/properties.rb
@@ -2,7 +2,13 @@ class Importer::Leboncoin::Properties
   def perform
     options = {}
     options[:postal_codes] = ["69001", "69002", "69003", "69006", "69007"]
-    properties = Fetcher::Leboncoin::Properties.new(options).perform
+    begin
+      count = 0
+      properties = Fetcher::Leboncoin::Properties.new(options).perform
+    rescue => Net::ReadTimeout
+      count += count
+      retry if count < 5
+    end
     properties.each do |property|
       Persister::Leboncoin::Properties.new(property).perform
     end
