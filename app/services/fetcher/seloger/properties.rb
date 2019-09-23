3617599c7224d47f549caa6eec7badab8bf1bfc9
dm0lz <ducrouxolivier@gmail.com>
2019-09-23 21:39:00 +0200

    updating fetch requests

--- app/services/fetcher/seloger/properties.rb
+++ app/services/fetcher/seloger/properties.rb
@@ -2,7 +2,7 @@ class Fetcher::Seloger::Properties
   def initialize(options = {})
   end
   def perform(page_nb)
-    url = "http://ws-seloger.svc.groupe-seloger.com/search.xml?idtt=2&ci=690381,690382,690383,690386,690387&pxmax=200000&SEARCHpg=#{page_nb}"
+    url = "http://ws-seloger.svc.groupe-seloger.com/search.xml?idtt=2&ci=690381,690382,690383,690384,690385,690386,690387,690388,6903839&pxmax=200000&SEARCHpg=#{page_nb}"
     response = Faraday.get(url)
     response.body
   end
--- app/services/persister/leboncoin/properties.rb
+++ app/services/persister/leboncoin/properties.rb
@@ -6,28 +6,32 @@ class Persister::Leboncoin::Properties
   attr_reader :property
 
   def perform
-    content = {
-      external_id: property['list_id'],
-      first_publication_date: property['first_publication_date'],
-      index_date: property['index_date'],
-      status: property['status'],
-      category_id: property['category_id'],
-      category_name: property['category_name'],
-      subject: property['subject'],
-      body: property['body'],
-      ad_type: property['ad_type'],
-      url: property['url'],
-      price: property['price'].first,
-      surface: property['attributes'].select{|l| l["key"] == 'square'}.first['value'],
-      postal_code: property["location"]["zipcode"],
-      pictures: property["images"]["urls_thumb"],
-      external_provider: 'leboncoin',
-      real_estate_type: retrieve_real_estate_type(property),
-      original_payload: property
-    }
-    listing = Listing.find_or_initialize_by(external_id: property['list_id'])
-    listing.update(content)
-    listing.save!
+    # begin
+      content = {
+        external_id: property['list_id'],
+        first_publication_date: property['first_publication_date'],
+        index_date: property['index_date'],
+        status: property['status'],
+        category_id: property['category_id'],
+        category_name: property['category_name'],
+        subject: property['subject'],
+        body: property['body'],
+        ad_type: property['ad_type'],
+        url: property['url'],
+        price: property['price'].first,
+        surface: retrieve_surface(property),
+        postal_code: property["location"]["zipcode"],
+        pictures: property["images"]["urls_thumb"],
+        external_provider: 'leboncoin',
+        real_estate_type: retrieve_real_estate_type(property),
+        original_payload: property
+      }
+      listing = Listing.find_or_initialize_by(external_id: property['list_id'])
+      listing.update(content)
+      listing.save!
+    # rescue => e
+    #   puts e
+    # end
   end
 
   def retrieve_real_estate_type(property)
@@ -36,4 +40,7 @@ class Persister::Leboncoin::Properties
     real_estate_attribute.first['value_label'].downcase
   end
 
+  def retrieve_surface(property)
+    property['attributes'].select{|l| l["key"] == 'square'}.first['value'] rescue nil
+  end
 end
