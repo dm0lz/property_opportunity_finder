7530fd2724a6ae00b555250c57fd6f03da2dee14
dm0lz <ducrouxolivier@gmail.com>
2019-10-06 20:53:39 +0200

    cities csv

--- app/services/insee_location_importer.rb
+++ app/services/insee_location_importer.rb
@@ -1,7 +1,7 @@
 require 'csv'
 class InseeLocationImporter
   def perform
-    csv = File.read('/Users/olivier/Downloads/laposte_hexasmal.csv')
+    csv = File.read File.join(Rails.root, 'public', 'laposte_hexasmal.csv')
     CSV.parse(csv, :headers => true) do |row|
       data_array = row["Code_commune_INSEE;Nom_commune;Code_postal;Libelle_acheminement;Ligne_5;coordonnees_gps"].split(';')
       insee_location = InseeLocation.create(insee_code: data_array[0], postal_code: data_array[2], name: data_array[3])
